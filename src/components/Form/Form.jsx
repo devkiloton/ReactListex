d60e41a2120d0197a7136803171619e958bc854c
devkiloton <dev.kiloton@gmail.com>
2021-10-27 12:52:53 -0300

    saving title and text strings

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,11 +2,42 @@ import react from "react";
 import { Component } from "react";
 import './style.css'
 export default class Form extends Component{
+
+    constructor(){
+        super();
+        this.title='';
+        this.text='';
+    }
+
+    _handleChangeTitle(event){
+        event.stopPropagation();
+        this.title=event.target.value;
+    }
+
+    _handleChangeText(event){
+        event.stopPropagation();
+        this.text=event.target.value;
+    }
+
+    _createNote(event){
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
     render(){
         return(
-        <form className='form-note'>
-            <input type='text' placeholder='Title'/>
-            <textarea placeholder='Write something here...'/>
+        <form
+            onSubmit={this._createNote.bind(this)} 
+            className='form-note'>
+            <input 
+                type='text' 
+                placeholder='Title'
+                onChange={this._handleChangeTitle.bind(this)}
+                />
+            <textarea 
+                placeholder='Write something here...'
+                onChange={this._handleChangeText.bind(this)}
+                />
             <button>Send</button>
         </form>
         )
